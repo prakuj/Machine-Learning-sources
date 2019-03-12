dc0874c6f0a1085e2e76d085325ff3eb0ff8a884
i348491 <pankaj.kumar11@sap.com>
2019-03-13 00:07:43 +0530

    adding pandas overview

---
+++ libraries and packages/pandas.py
@@ -0,0 +1,20 @@
+# pandas is a
+	# Python library 
+	# providing high-performance, easy-to-use data structures and 
+	# data analysis tools
+
+
+# Pandas deals with the following three data structures −
+	# Series
+	# DataFrame
+	# Panel
+# These data structures are built on top of Numpy array, which means they are fast.
+
+Data Structure		Dimensions		Homo/hetro			value-mutable 		size-mutable
+Series					1			homogeneous array 	  Yes					No
+Data Frames				2			hetro 				  Yes 					Yes
+Panel					3			hetro 				  Yes					yes
+
+# All Pandas data structures are value mutable (can be changed)
+# All pandas ds except Series all are size mutable. Series is size immutable.
+
